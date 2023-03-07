e9092766a99fb74500a2df913b91bf704a8fc02f
AleksDamsky <aahripunov@mail.ru>
2023-03-07 16:25:33 +0400

    I decided more exercises

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ function boolToWord( bool ){
   return bool ? 'Yes' : 'No';
 }
 boolToWord(true)
+
+//Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+    //   let arr = str.split()
+      return str.trim().split(" ").reverse().join(" ");
+    }
+    
+    nameShuffler('john McClane');
+
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+// Function should return true if it is possible and false if not.
+
+const zeroFuel = (pump, mpg, fuelLeft) => {
+  return (pump > (mpg * fuelLeft)) ? false : true;
+};
+zeroFuel()
+
