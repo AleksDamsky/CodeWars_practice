2f9777e7b88009737502044c9383aa2dc36871df
AleksDamsky <aahripunov@mail.ru>
2023-03-10 11:57:40 +0400

    I decide an exercises

--- index.js
+++ index.js
@@ -166,3 +166,20 @@ function test4(min, max){
 let myRandomNum = test4(0, 100);
 
 console.log(myRandomNum())
+// Task Overview:
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+// If there is enough space, return 0, and if there isn't, return the number of passengers he can't take.
+
+function enough(cap, on, wait) {
+  if (cap >= on - wait){
+  return 0;
+  }
+
+  return (on + wait) - cap;
+}
+
+enough(100, 32, 88);
